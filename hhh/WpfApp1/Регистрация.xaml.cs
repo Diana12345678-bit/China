4cdfa0d2a2660230f44aa2ad98de5271fda49fec
Мушинская Диана Антоновна <10230915@college.local>
2025-11-28 14:01:08 +0300

    Регистрация

--- hhh/WpfApp1/Регистрация.xaml.cs
+++ hhh/WpfApp1/Регистрация.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp1
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Регистрация.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Регистрация : Window
     {
-        public MainWindow()
+        public Регистрация()
         {
             InitializeComponent();
         }
